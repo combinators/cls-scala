77bc7165527ffb57bfe65549e7db8e1e69c146ec
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2016-07-26 15:18:10 +0200

    Add example project

--- build.sbt
+++ build.sbt
@@ -4,6 +4,15 @@ version := "1.0"
 resolvers += Resolver.sonatypeRepo("releases")
 resolvers += Resolver.typesafeRepo("releases")
 
+lazy val exampleProject =
+  (project in file("./example"))
+    .dependsOn(root)
+    .enablePlugins(SbtTwirl)
+    .settings(
+      scalaVersion := "2.11.8",
+      sourceDirectories in (Compile, TwirlKeys.compileTemplates) := (unmanagedSourceDirectories in Compile).value
+    )
+
 lazy val root = (project in file("."))
   .settings(
     scalaVersion := "2.11.8",
---
+++ example/src/main/scala/newsticker.scala
@@ -0,0 +1,83 @@
+import java.nio.charset.Charset
+import java.nio.file.{Files, Paths}
+
+import de.tu_dortmund.cs.ls14.cls.types._
+import de.tu_dortmund.cs.ls14.cls.inhabitation._
+import de.tu_dortmund.cs.ls14.cls.interpreter._
+import play.twirl.api.Html
+import syntax._
+import ReflectedRepository._
+
+object Newsticker extends App {
+
+  val alpha = Variable("alpha")
+  val beta = Variable("beta")
+
+  trait MessageRepository {
+    @combinator object InhabitationRocks {
+      def apply: Html = Html("<h1>Inhabitation Rocks</h1>")
+      val semanticType = 'TrueMessage
+    }
+    @combinator object Productive {
+      def apply: Html = Html("<h1>Inhabitation makes developers super productive!</h1>")
+      val semanticType = 'TrueMessage
+    }
+  }
+
+  trait HeadlineRepository {
+    @combinator object InhabitationNews {
+      def apply: String = "Inhabitation News"
+      val semanticType = 'Headline
+    }
+  }
+
+  trait UtilRepository {
+    @combinator object append {
+      def apply(message: Html, messages: List[Html]): List[Html] = message +: messages
+      val semanticType = alpha =>: 'List(alpha) =>: 'List(alpha) :&: 'NonEmpty
+    }
+    @combinator object emptyList {
+      def apply: List[Html] = List.empty
+      val semanticType = 'List(alpha) :&: 'Empty
+    }
+  }
+
+  trait Repository extends MessageRepository with HeadlineRepository with UtilRepository {
+    @combinator object newsticker {
+      def apply(theTitle: String, messages: List[Html]): Html = templates.newsticker(theTitle, messages)
+      val semanticType = 'Headline =>: 'List('Message) :&: beta =>: 'Newsticker :&: beta
+    }
+  }
+
+  val semanticTaxonomy =
+    Taxonomy("Message")
+      .addSubtype("TrueMessage")
+      .merge(
+        Taxonomy("ListCharacteristic")
+          .addSubtype("Empty")
+          .addSubtype("NonEmpty")
+      )
+  val kinding =
+    Kinding(alpha)
+      .addOption('Message)
+      .merge(
+        Kinding(beta)
+          .addOption('Empty)
+          .addOption('NonEmpty)
+      )
+
+  val Gamma = new Repository {}
+  val reflectedGamma = ReflectedRepository(Gamma)
+  val fullTaxonomy = reflectedGamma.nativeTypeTaxonomy.merge(semanticTaxonomy)
+  val algorithm = new BoundedCombinatoryLogic(kinding, new SubtypeEnvironment(fullTaxonomy), reflectedGamma.combinators)
+
+  val target = nativeTypeOf[Html] :&: 'Newsticker :&: 'NonEmpty
+  val solutions = TreeGrammarEnumeration(algorithm.inhabit(target), target)
+
+  for (i <- 1 to 10) {
+    val file = Paths.get("newsticker_$i.html")
+    val content = new java.util.ArrayList[String]()
+    content.add(solutions.index(i).toString)
+    Files.write(file, content, Charset.forName("UTF-8"))
+  }
+}
